5039cc0e4dd733c147146b57db625d06d76a4563
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2022-07-19 11:07:48 +0800

    refactor

--- server/index.js
+++ server/index.js
@@ -1,48 +1,18 @@
-const { ApolloServer, gql } = require('apollo-server')
-
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-
-const books = [
-    {
-        title: 'Harry Potter and the Chamber of Secrets',
-    },
-    {
-        title: 'Jurassic Park',
-    },
-    {
-        title: 'The Hobbit',
-    },
-]
-
-const typeDefs = gql`
-    # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-    # This "Book" type defines the queryable fields for every book in our data source.
-    type Book {
-        title: String
-        author: String
-    }
-
-    # The "Query" type is special: it lists all of the available queries that
-    # clients can execute, along with the return type for each. In this
-    # case, the "books" query returns an array of zero or more Books (defined above).
-    type Query {
-        books: [Book]
-    }
-`
-
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
-const resolvers = {
-    Query: {
-        books: () => books,
-    },
-}
+const { ApolloServer } = require('apollo-server')
+const mongoose = require('mongoose')
+const uri =
+    'mongodb+srv://kobekoblanca:Chixxmagnet00@cluster0.kcbgjsu.mongodb.net/MERNG?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 3000
+const typeDefs = require('./graphql/typedefs')
+const resolvers = require('./graphql/resolvers')
 
 const server = new ApolloServer({ typeDefs, resolvers })
 
-server.listen(3000).then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`)
+mongoose
+    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to database!')
+    })
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
