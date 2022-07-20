5889017f1ef94322a4326024c6eea2db7aaf966d
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2022-07-20 22:25:48 +0800

    starting frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App 2xl:mx-96   xl:mx-80   lg:mx-72 md:mx-12">
+            <div className="flex w-full border-b-4 justify-center items-center">
+                <div className="flex w-full  justify-between px-7 font-medium">
+                    <div className="p-5 cursor-pointer">Home</div>
+                    <div className="flex">
+                        <ul className="flex ">
+                            <li className="p-5 cursor-pointer">Sign in</li>
+                            <li className="p-5 cursor-pointer">Register</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="h-screen bg-slate-100 mt-5">
+                <div>
+                    <h1>Heading</h1>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
